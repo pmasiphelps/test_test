d31be38a222749463dffdeee8d0488d559904e5d
admin <admin>
2020-09-24 18:53:56 -0400

    Recipe APIEXAMPLES.compute_out_py saved through notebook notebook editor for compute_out_py

--- recipes/compute_out_py.py
+++ recipes/compute_out_py.py
@@ -1,13 +1,18 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 ds_with_arrays = dataiku.Dataset("ds_with_arrays")
 ds_with_arrays_df = ds_with_arrays.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+ds_with_arrays_df['array_col']
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
