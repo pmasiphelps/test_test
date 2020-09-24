498b4df86ce5d8e3b1f7aca768f45c962a5d2f1c
admin <admin>
2020-09-24 18:53:15 -0400

    Saved recipe: APIEXAMPLES.compute_out_py

---
+++ recipes/compute_out_py.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+ds_with_arrays = dataiku.Dataset("ds_with_arrays")
+ds_with_arrays_df = ds_with_arrays.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+out_py_df = ds_with_arrays_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+out_py = dataiku.Dataset("out_py")
+out_py.write_with_schema(out_py_df)
